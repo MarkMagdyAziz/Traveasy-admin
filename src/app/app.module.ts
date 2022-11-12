406a184a2519ba1abb08171c3b0b743ef39f8227
aymanyori <aymanyori00@gmail.com>
2022-11-13 00:36:24 +0200

    Add Sliderbar + Ui Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { LogginComponent } from './components/loggin/loggin.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UsersComponent } from './components/users/users.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { FlightsComponent } from './components/flights/flights.component';
+import { HolidayComponent } from './components/holiday/holiday.component';
+import { TourguidComponent } from './components/tourguid/tourguid.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
-  declarations: [AppComponent, LogginComponent, RegisterComponent],
+  declarations: [AppComponent, LogginComponent, RegisterComponent, DashboardComponent, SidebarComponent, UsersComponent, HotelsComponent, FlightsComponent, HolidayComponent, TourguidComponent, BodyComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
