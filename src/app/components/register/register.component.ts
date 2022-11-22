ef4cb3d4164356108672b46642c515ff0a722f34
Mark M Aziz <mark.aziz.magdy@gmail.com>
2022-11-22 02:11:00 +0200

    guard pugs and roles pug solved

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -4,6 +4,7 @@ import { AuthAPIServiceService } from 'src/app/services/auth-apiservice.service'
 import { IUser } from '../../interfaces/iuser';
 import { StorageService } from 'src/app/services/storage.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +20,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private authService: AuthAPIServiceService,
     private storageService: StorageService,
-    private router: Router
+    private router: Router,
+    private toastr: ToastrService
   ) { }
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -64,10 +66,9 @@ export class RegisterComponent implements OnInit {
         console.log(err);
         this.isSignUpFailed = true;
       },
-      complete: () => this.router.navigate(['/login']),
+      complete: () => this.toastr.success('Added Successfuly!'),
     };
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value)
       this.authService.register(this.registerForm.value).subscribe(observer);
     }
   }
